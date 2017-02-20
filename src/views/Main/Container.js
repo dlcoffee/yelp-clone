2a1dcf72c81539c4cdf7b991d9ed4950fcba6bf6
Daniel Tea <daniel_tea@spe.sony.com>
2017-02-20 10:59:38 -0800

    Sidebar component

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -3,6 +3,7 @@ import Map, {GoogleApiWrapper} from 'google-maps-react';
 import {searchNearby} from 'utils/googleApiHelpers';
 
 import Header from 'components/Header/Header';
+import Sidebar from 'components/Sidebar/Sidebar';
 
 import styles from './styles.module.css'
 
@@ -48,6 +49,7 @@ export class Container extends React.Component {
           visible={false}>
 
           <Header />
+          <Sidebar title={'Restaurants'} places={this.state.places}/>
           {this.state.places.map(place => {
             return (
               <div key={place.id}>{place.name}</div>
